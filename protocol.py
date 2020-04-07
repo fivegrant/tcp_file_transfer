593b83217636cef9ec20a6d5077d2d9e4026bef8
satchel baldwin <satchelbaldwin@gmail.com>
2020-04-06 20:22:38 -0400

    Change protocol to consts from dict

--- protocol.py
+++ protocol.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
  protocol.py - A set of dictionaries that make interfacing with
                protocol easier.
@@ -6,17 +8,37 @@
 """
 #DEVELOPMENT: THIS MODULE MAY BE REMOVED LATER IF NOT NECESSARY
 
-decode = { 0x01: "handshake"
-         , 0x02: "send"
-         , 0x03: "end"
-         , 0x10: "bof"
-         , 0x20: "eof"
-         , 0xF0: "success"
-         , 0xF1: "modal mismatch"
-         , 0xF2: "bad filename"
+# rationale: magic strings as constants is okay
+#   but consider redundant calls to encode/decode instead of
+#   leaving the constant as-is and using those for human-facing
+#   messages like errors; benefit to naming is if we ever need
+#   to change the exact hex values that this would be much easier
+#   as there'd only be one copy
+# -s 4/6
+
+# messages
+HANDSHAKE    = 0x01
+BEGIN_SEND   = 0x02 
+END_SEND     = 0x03
+BEGIN_FILE   = 0x10
+END_FILE     = 0x20
+CONN_SUCCESS = 0xf0
+
+# errors
+ERR_MODE     = 0xF1
+ERR_FILENAME = 0xF2
+
+# swap semantics to human readable names for errors
+# -s
+decode = { HANDSHAKE:    "handshake"
+         , BEGIN_SEND:   "send"
+         , END_SEND:     "end"
+         , BEGIN_FILE:   "bof"
+         , END_FILE:     "eof"
+         , CONN_SUCCESS: "success"
+         , ERR_MODE:     "ERROR: Modal mismatch between client and server"
+         , ERR_FILENAME: "ERROR: Bad filename (non ascii text or filename > 256 chars)"
          }
 
 # Swap key: value -> value: key
 encode = { word : hexcode for (hexcode, word) in decode.items() }
-
-         
